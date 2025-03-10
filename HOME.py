c7b22f24c831b56edb11697db133ce52bdbf41e2
MK <99416359+MK316@users.noreply.github.com>
2025-03-10 15:52:52 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -10,10 +10,19 @@ st.caption(
     Welcome to the Chemistry Learning Apps! This platform is designed to help future chemistry teachers understand how to integrate technology into their teaching. This platform was created to demonstrate how Streamlit can be used to build educational apps for future chemistry teachers. (Since Mar. 10, 2025)  
     """)
 
-# Add an image or logo (Optional)
-st.image("https://github.com/MK316/chemistry/raw/main/images/chemistry01.png", 
-         caption="Enhance Chemistry Learning with Teacher-designed Applications", 
-         width=300)  # Adjust width value as needed
+# ✅ Add images using columns
+col1, col2 = st.columns([1, 1])  # Divide into two equal columns
+
+with col1:
+    st.image("https://github.com/MK316/chemistry/raw/main/images/chemistry01.png", 
+             caption="Enhance Chemistry Learning with Teacher-designed Applications", 
+             width=300)  # Adjust width value as needed
+
+with col2:
+    st.image("https://github.com/MK316/chemistry/raw/main/images/chemi25.png", 
+             caption="Explore Molecular Structures with Ease", 
+             width=100)  # Adjust width value as needed
+
 
 st.markdown("---")
 # Overview of available tools
