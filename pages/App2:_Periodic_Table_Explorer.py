ce3348f29c84be5cc309eb348ce43b668965e567
MK <99416359+MK316@users.noreply.github.com>
2025-03-10 14:19:11 +0900

    Update App2:_Periodic_Table_Explorer.py

--- pages/App2:_Periodic_Table_Explorer.py
+++ pages/App2:_Periodic_Table_Explorer.py
@@ -2,21 +2,28 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Load periodic table data
-df = pd.read_csv('https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableCSV.csv')
+tabs = st.tabs(["🔎 App overview", "🐾 Practice with APP"])
 
-# Create a scatter plot for the periodic table
-fig = px.scatter(df, x="xpos", y="ypos", text="symbol", color="category",
-                 hover_name="name", size_max=40, width=900, height=400)
+with tabs[0]:
+    st.caption("Overview")
 
-fig.update_traces(marker=dict(size=30), textposition='middle center')
-
-st.plotly_chart(fig)
-
-# Display details when an element is selected
-selected_element = st.selectbox("Choose an element", df['name'])
-if selected_element:
-    element_data = df[df['name'] == selected_element].iloc[0]
-    st.write(element_data)
-
-st.info("Data source from https://github.com/Bowserinator/")
+with tabs[1]:
+  
+    # Load periodic table data
+    df = pd.read_csv('https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableCSV.csv')
+    
+    # Create a scatter plot for the periodic table
+    fig = px.scatter(df, x="xpos", y="ypos", text="symbol", color="category",
+                     hover_name="name", size_max=40, width=900, height=400)
+    
+    fig.update_traces(marker=dict(size=30), textposition='middle center')
+    
+    st.plotly_chart(fig)
+    
+    # Display details when an element is selected
+    selected_element = st.selectbox("Choose an element", df['name'])
+    if selected_element:
+        element_data = df[df['name'] == selected_element].iloc[0]
+        st.write(element_data)
+    
+    st.info("Data source from https://github.com/Bowserinator/")
