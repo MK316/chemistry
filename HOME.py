732a67d3c74936f4a0813c3a5112ec4433b882e1
MK <99416359+MK316@users.noreply.github.com>
2025-03-10 14:48:57 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -10,6 +10,8 @@ st.caption(
     Welcome to the Chemistry Learning Apps! This platform is designed to help future chemistry teachers understand how to integrate technology into their teaching.  
     """
 )
+st.caption("This platform was created to demonstrate how Streamlit can be used to build educational apps for future chemistry teachers.")
+st.markdown("---")
 # Add an image or logo (Optional)
 st.image("https://github.com/MK316/chemistry/raw/main/images/chemistry01.png", 
          caption="Enhance Chemistry Learning with Technology", 
@@ -24,5 +26,5 @@ st.markdown("3. **Chemical Equation Balancer** – Automatically balance chemica
 st.markdown("4. **pH and Acid-Base Indicator** – To understand acids, bases, and neutral solutions based on pH levels.  ")
 st.markdown("5. **States of Matter Simulation** – To visualize how molecules behave in different states of matter.")
 # Footer
-st.caption("This platform was created to demonstrate how Streamlit can be used to build educational apps for future chemistry teachers.")
+
 
